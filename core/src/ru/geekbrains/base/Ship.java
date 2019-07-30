61a2e45bd078b5a58f537ed07af6a4642e181059
dyakubov <tuborg8944@gmail.com>
2019-07-30 15:28:41 +0300

    Lesson 7

--- core/src/ru/geekbrains/base/Ship.java
+++ core/src/ru/geekbrains/base/Ship.java
@@ -6,13 +6,16 @@ import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.pool.BulletPool;
+import ru.geekbrains.pool.ExplosionPool;
 import ru.geekbrains.sprite.Bullet;
+import ru.geekbrains.sprite.Explosion;
 
 public abstract class Ship extends Sprite {
 
     protected TextureRegion bulletRegion;
     protected Rect worldBounds;
     protected BulletPool bulletPool;
+    protected ExplosionPool explosionPool;
 
     protected Vector2 v;
     protected Vector2 v0;
@@ -23,16 +26,26 @@ public abstract class Ship extends Sprite {
     protected float bulletHeight;
 
     protected Sound shootSound;
-    protected Sound explosionSound;
 
     protected int damage;
     protected int hp;
+
+    private final float damageAnimateInterval = 0.1f;
+    private float damageAnimateTimer = damageAnimateInterval;
 
     public Ship() {
     }
 
     public Ship(TextureRegion region, int rows, int cols, int frames) {
         super(region, rows, cols, frames);
+    }
+
+    @Override
+    public void update(float delta) {
+        damageAnimateTimer += delta;
+        if (damageAnimateTimer >= damageAnimateInterval) {
+            frame = 0;
+        }
     }
 
     @Override
@@ -45,17 +58,34 @@ public abstract class Ship extends Sprite {
         shootSound.dispose();
     }
 
-    public void shoot() {
-        Bullet bullet = bulletPool.obtain();
-        bullet.set(this, bulletRegion, pos, bulletV, bulletHeight, worldBounds, damage);
-        shootSound.play();
+    public void damage(int damage) {
+        frame = 1;
+        damageAnimateTimer = 0f;
+        hp -= damage;
+        if (hp <= 0) {
+            destroy();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        hp = 0;
+        boom();
+        super.destroy();
     }
 
     public int getHp() {
         return hp;
     }
 
-    public void setHp(int hp) {
-        this.hp = hp;
+    protected void shoot() {
+        Bullet bullet = bulletPool.obtain();
+        bullet.set(this, bulletRegion, pos, bulletV, bulletHeight, worldBounds, damage);
+        shootSound.play();
+    }
+
+    private void boom() {
+        Explosion explosion = explosionPool.obtain();
+        explosion.set(getHeight(), pos);
     }
 }
--- core/src/ru/geekbrains/sprite/Enemy.java
+++ core/src/ru/geekbrains/sprite/Enemy.java
@@ -7,17 +7,20 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.base.Ship;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.pool.BulletPool;
+import ru.geekbrains.pool.ExplosionPool;
 
 public class Enemy extends Ship {
 
     private enum State {DESCENT, FIGHT}
+
     private State state;
+
     private Vector2 descentV = new Vector2(0, -0.15f);
 
-    public Enemy(BulletPool bulletPool, Rect worldBounds) {
+    public Enemy(BulletPool bulletPool, ExplosionPool explosionPool, Rect worldBounds) {
         shootSound = Gdx.audio.newSound(Gdx.files.internal("sounds/bullet.wav"));
-        explosionSound = Gdx.audio.newSound(Gdx.files.internal("sounds/explosion.wav"));
         this.bulletPool = bulletPool;
+        this.explosionPool = explosionPool;
         this.worldBounds = worldBounds;
         v = new Vector2();
         v0 = new Vector2();
@@ -27,6 +30,7 @@ public class Enemy extends Ship {
 
     @Override
     public void update(float delta) {
+        super.update(delta);
         pos.mulAdd(v, delta);
         switch (state) {
             case DESCENT:
@@ -41,12 +45,7 @@ public class Enemy extends Ship {
                     reloadTimer = 0f;
                     shoot();
                 }
-                if (hp <= 0){
-                    destroy();
-                    explosionSound.play();
-                }
-
-                if (getBottom() < worldBounds.getBottom()){
+                if (getBottom() < worldBounds.getBottom()) {
                     destroy();
                 }
                 break;
@@ -77,4 +76,14 @@ public class Enemy extends Ship {
         v.set(descentV);
         state = State.DESCENT;
     }
+
+    public boolean isBulletCollision(Rect bullet) {
+        return !(
+                bullet.getRight() < getLeft()
+                        || bullet.getLeft() > getRight()
+                        || bullet.getBottom() > getTop()
+                        || bullet.getTop() < pos.y
+        );
+    }
+
 }
